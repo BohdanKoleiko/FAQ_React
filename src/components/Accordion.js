7c14504acb4d3a168b5262348c743c8578e2688c
HamaHs <lusuy1995@gmail.com>
2024-02-04 02:04:30 +0100

    add logick to get accordionBody height

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,8 +1,15 @@
+import { useRef, useState, useEffect } from "react";
 import { CgCloseO } from "react-icons/cg";
 import styles from "./Accordion.module.scss";
 
 const Accordion = function (props) {
    const { FAQList, switchVisibility } = props;
+   const [accordBodyHeight, setAccordBodyHeight] = useState([]);
+   const accordionBodiesRef = useRef([]);
+
+   useEffect(() => {
+      setAccordBodyHeight(accordionBodiesRef.current);
+   }, []);
 
    return FAQList.map((faq, i) => (
       <div className={styles.accordion} key={i}>
@@ -23,9 +30,12 @@ const Accordion = function (props) {
             className={`${styles.accordionCollapsed} ${
                faq.visible ? styles.accordionCollapsed_show : ""
             }`}
-            //style={faq.visible ? { height: `${0}px` } : { height: `0px` }}
+            style={faq.visible ? { height: `${accordBodyHeight[i]}px` } : { height: `0px` }}
          >
-            <div className={styles.accordionBody} data-visible={faq.visible ? true : false}>
+            <div
+               className={styles.accordionBody}
+               ref={(el) => (accordionBodiesRef.current[i] = el?.scrollHeight)}
+            >
                {faq.text}
             </div>
          </div>
